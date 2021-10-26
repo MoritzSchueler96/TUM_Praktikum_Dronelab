62d8f1c0ad3639b862fc4bad32d950ab0c8fb398
Markus Wehn <ge65kihytum.de>
2021-10-26 17:50:21 +0200

    add instructions of use

--- src/arp_node.cpp
+++ src/arp_node.cpp
@@ -100,6 +100,54 @@ int main(int argc, char **argv)
     auto droneStatus = autopilot.droneStatus();
 
     auto droneBattery=autopilot.droneBattery();
+      // render image, if there is a new one available
+      if(subscriber.getLastImage(image)) {
+
+          // TODO: add overlays to the cv::Mat image, e.g. text
+          //generate Text for drone state and add it to picture
+          cv::Size image_size= image.size();
+          std::string display="state: "+std::to_string(droneStatus);
+          cv::putText(image, display, cv::Point(10, 25), cv::FONT_HERSHEY_SIMPLEX,1, cv::Scalar(0,255,0), 2, false); //putText( image, text, org, font, fontScale, color, thickness, lineType, bottomLeftOrigin)
+          //creates text of Batterie charge in 0.1% and add it to picture
+          std::stringstream stream;
+          stream<< std::fixed<<std::setprecision(1)<<droneBattery <<"%";
+          auto color= cv::Scalar(0, 255,0);
+          cv::putText(image, stream.str(), cv::Point(image_size.width-100, 25), cv::FONT_HERSHEY_SIMPLEX,1, color, 2, false);
+          if(droneBattery<10){
+              color= cv::Scalar(255,0,0);
+          }
+          color=cv::Scalar(0, 255,0);
+          // possible commands in buttom of picture, differentiate: drone is flying or not
+          if(droneStatus==3||droneStatus==4||droneStatus==7) {
+              cv::putText(image, "W/ S: up/ down", cv::Point(5, image_size.height-30), cv::FONT_HERSHEY_SIMPLEX,0.5, color, 2, false);
+              cv::putText(image, "A/ D: yaw left/ right", cv::Point(5, image_size.height-5), cv::FONT_HERSHEY_SIMPLEX,0.5, color, 2, false);
+              cv::putText(image, "^/ v: for-/ backward", cv::Point(image_size.width-185, image_size.height-30), cv::FONT_HERSHEY_SIMPLEX,0.5, color, 2, false);
+              cv::putText(image, "</ >: left/ right", cv::Point(image_size.width-185, image_size.height-5), cv::FONT_HERSHEY_SIMPLEX,0.5, color, 2, false);
+              cv::putText(image, "L: Landing; ESC: Stop", cv::Point(image_size.width/2-100, image_size.height-5), cv::FONT_HERSHEY_SIMPLEX,0.5, color, 2, false);
+
+          }
+          else if(droneStatus==2){
+              cv::putText(image, "T: Tacking off; ESC: Stop", cv::Point(image_size.width/2-105, image_size.height-5), cv::FONT_HERSHEY_SIMPLEX,0.5, color, 2, false);
+          }
+          else
+          {
+              cv::putText(image, "ESC: Stop", cv::Point(image_size.width/2-50, image_size.height-5), cv::FONT_HERSHEY_SIMPLEX,0.5, color, 2, false);
+          }
+
+
+          // https://stackoverflow.com/questions/22702630/converting-cvmat-to-sdl-texture
+          // I'm using SDL_TEXTUREACCESS_STREAMING because it's for a video player, you should
+          // pick whatever suits you most: https://wiki.libsdl.org/SDL_TextureAccess
+          // remember to pick the right SDL_PIXELFORMAT_* !
+          texture = SDL_CreateTexture(
+                  renderer, SDL_PIXELFORMAT_BGR24, SDL_TEXTUREACCESS_STREAMING, image.cols, image.rows);
+          SDL_UpdateTexture(texture, NULL, (void*)image.data, image.step1());
+          SDL_RenderClear(renderer);
+          SDL_RenderCopy(renderer, texture, NULL, NULL);
+          SDL_RenderPresent(renderer);
+          // cleanup (only after you're done displaying. you can repeatedly call UpdateTexture without destroying it)
+          SDL_DestroyTexture(texture);
+      }
     // command
     if (state[SDL_SCANCODE_ESCAPE]) {
       std::cout << "ESTOP PRESSED, SHUTTING OFF ALL MOTORS status=" << droneStatus;
@@ -201,36 +249,7 @@ int main(int argc, char **argv)
 
         }
 
-      // render image, if there is a new one available
-      if(subscriber.getLastImage(image)) {
 
-          // TODO: add overlays to the cv::Mat image, e.g. text
-          //generate Text for drone state and add it to picture
-          cv::Size image_size= image.size();
-          std::string display="state: "+std::to_string(droneStatus);
-          cv::putText(image, display, cv::Point(10, 25), cv::FONT_HERSHEY_SIMPLEX,1, cv::Scalar(0,255,0), 2, false); //putText( image, text, org, font, fontScale, color, thickness, lineType, bottomLeftOrigin)
-          //creates text of Batterie charge in 0.1% and add it to picture
-          std::stringstream stream;
-          stream<< std::fixed<<std::setprecision(1)<<droneBattery <<"%";
-          auto color= cv::Scalar(0, 255,0);
-          if(droneBattery<10){
-              color= cv::Scalar(255,0,0);
-          }
-          cv::putText(image, stream.str(), cv::Point(image_size.width-100, 25), cv::FONT_HERSHEY_SIMPLEX,1, color, 2, false);
-
-          // https://stackoverflow.com/questions/22702630/converting-cvmat-to-sdl-texture
-          // I'm using SDL_TEXTUREACCESS_STREAMING because it's for a video player, you should
-          // pick whatever suits you most: https://wiki.libsdl.org/SDL_TextureAccess
-          // remember to pick the right SDL_PIXELFORMAT_* !
-          texture = SDL_CreateTexture(
-                  renderer, SDL_PIXELFORMAT_BGR24, SDL_TEXTUREACCESS_STREAMING, image.cols, image.rows);
-          SDL_UpdateTexture(texture, NULL, (void*)image.data, image.step1());
-          SDL_RenderClear(renderer);
-          SDL_RenderCopy(renderer, texture, NULL, NULL);
-          SDL_RenderPresent(renderer);
-          // cleanup (only after you're done displaying. you can repeatedly call UpdateTexture without destroying it)
-          SDL_DestroyTexture(texture);
-      }
     }
 
 
